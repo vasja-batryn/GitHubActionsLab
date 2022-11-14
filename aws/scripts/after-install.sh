895df74f0e2180432b531c2d686b0b895295a92c
vasja-batryn <vasjabat@icloud.com>
2022-11-14 23:20:04 +0200

    sdfef

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://githubactionslab-webappdeploymentbucket-1utua80nliqf9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war --request-payer requester
+aws --no-sign-request s3 cp s3://githubactionslab-webappdeploymentbucket-1utua80nliqf9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war --request-payer requester
 
 
 # Ensure the ownership permissions are correct.
